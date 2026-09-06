1cc43e5d99a1ae7737bf6b2e734ab6870e28244c
arrasheedcloud-hub <arrasheedcloud@gmail.com>
2026-09-06 09:06:34 +0300

    Add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.overtime.yemen',
+  appName: 'ساعاتي الإضافية',
+  webDir: 'dist'
+};
+
+export default config;
